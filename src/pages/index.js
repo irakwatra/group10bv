d746aea153be16c7cad99f0744c7ac619515b966
irakwatra <irakawatra@gmail.com>
2022-04-30 17:48:59 +0530

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Features from "../components/Features/Features"
 import Hero from "../components/Hero/Hero"
 import Layout from "../components/Layout/Layout"
 import Faq from "../components/Faq/Faq"
-import Carousel from "../components/carousel/carousel"
+import Carousel from "../components/Carousel/carousel"
 import Carousel2 from "../components/Carousel2/carousel2"
 
 const Index = () => {
